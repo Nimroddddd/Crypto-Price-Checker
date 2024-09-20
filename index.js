6cadbe400daabc334ad7be6345ca7f484764de3e
Amin <min123kabir@gmail.com>
2024-09-20 02:40:30 +0100

    Complete

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const port = 3000;
 const api_config = {
 	headers: { 'x-cg-demo-api-key': 'CG-xodBnwJDy2qNt32AL3VEtHdu'}
 };
+var data = {}
+var coinPrice = ''
+var addressString = ''
 
 const config = {
 	params: {
 		'ids': 'ethereum,solana,polygon-ecosystem-token',
 		'vs_currencies': 'usd'
-	}
+	},
+  headers: { 'x-cg-demo-api-key': 'CG-xodBnwJDy2qNt32AL3VEtHdu'}
 }
+
+
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,14 +29,30 @@ app.get("/", async (req, res) => {
   try {
 		const response = await axios.get("https://api.coingecko.com/api/v3/simple/price", config);
 		const result = response.data;
-		const data = new Prices(result.ethereum.usd, result.solana.usd, result['polygon-ecosystem-token'].usd)
-		res.render("index.ejs", {base: data})
-		console.log(req.body)
+		data = new Prices(result.ethereum.usd, result.solana.usd, result['polygon-ecosystem-token'].usd)
+		res.render("index.ejs", {base: data, price: coinPrice})
   } catch(error) {
 		console.log(error.message)
 		res.render()
 	}
 });
+
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  const param = new PriceSearch(req.body.address);
+  const searchConfig = {params: param};
+  console.log(req.body.address);
+  try {
+    const response = await axios.get('https://api.coingecko.com/api/v3/simple/token_price/' + req.body.chain, searchConfig);
+    const result = response.data;
+    console.log(coinPrice);
+    coinPrice = result[req.body.address].usd
+    res.redirect("/"); 
+  } catch(error) {
+    console.log(error.message);
+    res.redirect("/");
+  }
+})
 
 
 
@@ -42,4 +64,9 @@ function Prices (x,y,z) {
 	this.bitcoin = x;
 	this.solana = y;
 	this.polygon = z;
+}
+
+function PriceSearch (x) {
+  this.contract_addresses = x,
+  this.vs_currencies = 'usd'
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+var fuck = { '0x3b56a704c01d650147ade2b8cee594066b3f9421': { usd: 0.00898345 } }
+console.log(fuck['0x3b56a704c01d650147ade2b8cee594066b3f9421'])
+
+var shit = 'well'
