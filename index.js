80edcda2bb8e55138ed1c1beb7e9c56e5920730d
Amin <min123kabir@gmail.com>
2024-10-05 10:12:08 +0100

    added final touches

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
+import env from "dotenv"
 
 // declarations of variables
 const app = express();
 const port = 3000;
-const api_config = {
-	headers: { 'x-cg-demo-api-key': 'CG-xodBnwJDy2qNt32AL3VEtHdu'}
-};
 var data = {}
-var coinPrice = ''
+
+
+
+//middlewares
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+env.config();
+
 
 //default config for getting the prices of the three base coins from coingecko
 const config = {
@@ -18,14 +23,8 @@ const config = {
 		'ids': 'ethereum,solana,polygon-ecosystem-token',
 		'vs_currencies': 'usd'
 	},
-  headers: { 'x-cg-demo-api-key': 'CG-xodBnwJDy2qNt32AL3VEtHdu'}
+  headers: { 'x-cg-demo-api-key': process.env.API_KEY}
 }
-
-
-//middlewares
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: true }));
-
 
 
 //homepage
@@ -34,6 +33,7 @@ app.get("/", async (req, res) => {
 		const response = await axios.get("https://api.coingecko.com/api/v3/simple/price", config);
 		const result = response.data;
 		data = new Prices(result.ethereum.usd, result.solana.usd, result['polygon-ecosystem-token'].usd)
+    let coinPrice;
 		res.render("index.ejs", {base: data, price: coinPrice})
   } catch(error) {
 		console.log(error.message)
@@ -43,18 +43,25 @@ app.get("/", async (req, res) => {
 
 
 //uses middleware to collect form data from user, then submits the relevant data to coingecko api to return the coin price.
-app.post("/", async (req, res) => {
+app.post("/search", async (req, res) => {
   console.log(req.body);
   const param = new PriceSearch(req.body.address);
   const searchConfig = {params: param};
   console.log(req.body.address);
-  const tokenAddress = `${req.body.address}`
+  const reqChain = req.body.chain;
+  let coinPrice;
   try {
     const response = await axios.get('https://api.coingecko.com/api/v3/simple/token_price/' + req.body.chain, searchConfig);
     const result = response.data;
     console.log(result);
+    let tokenAddress;
+    if (reqChain === "solana") {
+      tokenAddress = req.body.address;
+    } else {
+      tokenAddress = req.body.address.toLowerCase()
+    }
     coinPrice = result[tokenAddress].usd
-    res.redirect("/"); 
+    res.render("index.ejs", {base: data, price: coinPrice}); 
   } catch(error) {
     console.log(error.message);
     res.redirect("/");
